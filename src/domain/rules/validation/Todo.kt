d2ff12a6a2123ad2a15c1bdd7143bc78cebb8fd3
axj1l59 <adam_johnston1@homedepot.com>
2019-02-03 14:13:26 -0500

    Removing unnecessary printlns

--- src/domain/rules/validation/Todo.kt
+++ src/domain/rules/validation/Todo.kt
@@ -4,12 +4,10 @@ import domain.entities.Todo
 import kotlin.reflect.full.memberProperties
 
 fun validateTodo(todo: Todo): Boolean {
-    println("validating todo $todo")
     for (prop in Todo::class.memberProperties) {
         if (prop.get(todo) === null) {
             throw IllegalArgumentException("${prop.name} can not be null")
         }
-        println("${prop.name} = ${prop.get(todo)}")
     }
     return true
 }
