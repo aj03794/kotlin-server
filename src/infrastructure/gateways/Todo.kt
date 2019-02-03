6e037cd514c936825c399ac4ce5029d36e3dccce
axj1l59 <adam_johnston1@homedepot.com>
2019-02-03 14:21:11 -0500

    Implementing getAll and delete

--- src/infrastructure/gateways/Todo.kt
+++ src/infrastructure/gateways/Todo.kt
@@ -4,12 +4,17 @@ import domain.gateways.TodoGatewayInterface
 import domain.entities.Todo
 
 class TodoGatewayImpl(val todos: ArrayList<Todo> = ArrayList<Todo>()): TodoGatewayInterface {
-    override fun delete() { println("adding todo to list") }
+    override fun delete(id: Int): Todo {
+        val todo = this.todos.find{ it.id === id }!!
+        this.todos.remove(todo)
+        return todo
+    }
     override fun add(todo: Todo): String {
         this.todos.add(todo)
-        println(this.todos)
         return "todo added"
     }
     override fun get() { println("adding todo to list") }
-    override fun getAll() { println("adding todo to list") }
+    override fun getAll(): ArrayList<Todo> {
+        return this.todos
+    }
 }
