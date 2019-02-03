e46190c309870fb32f86dd2ca390c439826ef235
axj1l59 <adam_johnston1@homedepot.com>
2019-02-03 12:26:45 -0500

    Starting myId at 0

--- src/domain/entities/Todo.kt
+++ src/domain/entities/Todo.kt
@@ -1,6 +1,6 @@
 package domain.entities
 
-var myId = 1
+var myId = 0
 
 data class Todo(val id: Int = myId++ , val name: String = "", val description: String = "", val completed: Boolean = false)
 
