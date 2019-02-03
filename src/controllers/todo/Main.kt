23652081b495139fba4ec4e63931695fc0c7e8e5
axj1l59 <adam_johnston1@homedepot.com>
2019-02-03 12:27:16 -0500

    deleteTodo()

--- src/controllers/todo/Main.kt
+++ src/controllers/todo/Main.kt
@@ -2,6 +2,7 @@ package controllers.todo
 
 import domain.entities.Todo
 import usecases.interactors.AddTodoInteractor
+import usecases.interactors.DeleteTodoInteractor
 import domain.gateways.TodoGatewayInterface
 
 class TodoController(
@@ -9,15 +10,25 @@ class TodoController(
     val validateTodo: (todo: Todo) -> Boolean
 ) {
     val addTodoInteractor = AddTodoInteractor(todoGateway)
+    val deleteTodoInteractor = DeleteTodoInteractor(todoGateway)
     fun addTodo(todo: Todo): String {
         try {
             validateTodo(todo)
-            print(todo)
             val result = addTodoInteractor.execute(todo)
             return result
         }
         catch(err: IllegalArgumentException) {
             return err!!.message.toString()
         }
+    }
+    fun deleteTodo(id: String): String {
+        var idAsInt: Int
+        try {
+            idAsInt = id.toInt()
+        }
+        catch(err: Throwable) {
+            return err!!.message.toString()
+        }
+        return "Deleted $idAsInt"
     }
 }
